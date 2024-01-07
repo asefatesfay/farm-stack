bd7b3599f197ccb53baf4d63e871e52a6a8980fd
Assefa Tesfay <assefa.tesfay@nordstrom.com>
2024-01-07 12:54:37 -0800

    Style home page

--- ui/src/app/page.js
+++ ui/src/app/page.js
@@ -1,6 +1,38 @@
-
-export default function Home() {
+import Image from "next/image";
+import styles from "./page.module.css";
+const Home = () => {
   return (
-    <h1>Home Page</h1>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative insights agency</h1>
+        <p className={styles.desc}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.{" "}
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact Us</button>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src="/brands.png"
+            alt="brands"
+            fill
+            className={styles.brandsImage}
+          />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/hero.gif" alt="hero" fill className={styles.heroImage} />
+      </div>
+    </div>
+  );
+};
+export default Home;
