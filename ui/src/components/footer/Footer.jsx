02a5c0ed9b1bd3052591771d0a6a7e5f6ba04669
Assefa Tesfay <assefa.tesfay@nordstrom.com>
2024-01-12 16:19:59 -0800

    Add about and contact pages

--- ui/src/components/footer/Footer.jsx
+++ ui/src/components/footer/Footer.jsx
@@ -1,7 +1,16 @@
+import styles from "./footer.module.css"
+import Image from "next/image"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        Insights
+      </div>
+      <div className={styles.text}>
+        Copyright © 2024 Creative insights, Inc. All rights reserved.
+      </div>
+    </div>
   )
 }
 
--- ui/src/components/navbar/Navbar.jsx
+++ ui/src/components/navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 
 import Link from "next/link";
+import Image from "next/image"
 import Links from "./links/Links";
 import styles from "./navbar.module.css"
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <Link className = {styles.logo} href="/">Insights</Link>
+      <Link className = {styles.logo} href="/">
+        <Image src="/logo-4.jpg" width={50} height={50} alt="logo" className={styles.logo} />
+      </Link>
       <Links />
     </div>
   );
